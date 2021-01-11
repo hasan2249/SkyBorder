539af453afb5132f87bf8324e234768cfdd5b5bf
hasan2249 <hasanjahjah224@gmail.com>
2021-01-11 13:03:07 +0200

    fix folder name
    there is no error on local server but it is rised on online server.

--- resources/views/backend/plans/create.blade.php
+++ resources/views/backend/plans/create.blade.php
@@ -3,14 +3,14 @@
 @section('title', __('labels.backend.access.Plans.management') . ' | ' . __('labels.backend.access.Plans.create'))
 
 @section('breadcrumb-links')
-    @include('backend.Plans.includes.breadcrumb-links')
+    @include('backend.plans.includes.breadcrumb-links')
 @endsection
 
 @section('content')
     {{ Form::open(['route' => 'admin.plans.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-permission', 'files' => true]) }}
 
     <div class="card">
-        @include('backend.Plans.form')
+        @include('backend.plans.form')
         @include('backend.components.footer-buttons', ['cancelRoute' => 'admin.plans.index'])
     </div><!--card-->
     {{ Form::close() }}
--- resources/views/backend/plans/edit.blade.php
+++ resources/views/backend/plans/edit.blade.php
@@ -3,14 +3,14 @@
 @section('title', __('labels.backend.access.Plans.management') . ' | ' . __('labels.backend.access.Plans.edit'))
 
 @section('breadcrumb-links')
-    @include('backend.Plans.includes.breadcrumb-links')
+    @include('backend.plans.includes.breadcrumb-links')
 @endsection
 
 @section('content')
     {{ Form::model($plan, ['route' => ['admin.plans.update', $plan], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'PATCH', 'id' => 'edit-role', 'files' => true]) }}
 
     <div class="card">
-        @include('backend.Plans.form')
+        @include('backend.plans.form')
         @include('backend.components.footer-buttons', [ 'cancelRoute' => 'admin.plans.index', 'id' => $plan->id ])
     </div><!--card-->
     {{ Form::close() }}
